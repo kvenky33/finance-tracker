577ce5d7d804fc62ede6f6d16e71d08f03cd2ead
konalam venkatesh <konalamvenkatesh111@gmail.com>
2025-02-09 13:57:17 +0530

    style

--- src/TransactionTable/index.js
+++ src/TransactionTable/index.js
@@ -1,8 +1,7 @@
 import React,{useState} from 'react'
 import { Table,Select,Radio } from 'antd';
+import searchImg from '../assets/search.svg'
 import './style.css'
-
-
 
 const TransactionTable = ({transactions}) => {
   const {Option} = Select
@@ -58,20 +57,33 @@ const TransactionTable = ({transactions}) => {
      
   return (
     <>
+    <div className='search-select'>
+      <div className='search-part'>
+        <label for="search"> <img src={searchImg} alt='search-icon' /></label>
+        <input value={search} onChange={(e)=>setSearch(e.target.value)} placeholder='Search by name' id='search'/>
+      </div>
+      <Select className='select-input' style={{width:"20%",boxShadow:" 0 0 8px 8px #f4f1f1", borderRadius:'8px'}} onChange={(value)=>setTypeFilter(value)} placeholder="Filter" value={typeFilter} allowClear>
+        <Option value="">All</Option>
+        <Option value="income">Income</Option>
+        <Option value="expense">Expense</Option>
+      </Select>
+    </div>
 
-    <input value={search} onChange={(e)=>setSearch(e.target.value)} placeholder='Search by name'/>
-    <Select className='select-input' onChange={(value)=>setTypeFilter(value)} placeholder="Filter" value={typeFilter} allowClear>
-      <Option value="">All</Option>
-      <Option value="income">Income</Option>
-      <Option value="expense">Expense</Option>
-    </Select>
-    <Radio.Group className='input-radio' onChange={(e)=>setSortKey(e.target.value)} value={sortKey}>
-    <Radio.Button value="">No Sort</Radio.Button>
-    <Radio.Button value="date">Sort by Date</Radio.Button>
-    <Radio.Button value="amount">Sort by Amount</Radio.Button>
-  </Radio.Group>
-
-    <Table dataSource={sortedTransaction} columns={columns} />;
+    <div className='myTransactions'>
+      <div className='myTransactions-headPart'>
+        <h3>My Transactions</h3>
+          <Radio.Group className='input-radio' onChange={(e)=>setSortKey(e.target.value)} value={sortKey}>
+            <Radio.Button value="">No Sort</Radio.Button>
+            <Radio.Button value="date">Sort by Date</Radio.Button>
+            <Radio.Button value="amount">Sort by Amount</Radio.Button>
+          </Radio.Group>
+          <div className='csv-btns'>
+            <button className='btn'>Export to CSV</button>
+            <button className='btn blue-btn'>Import from CSV</button>
+          </div>
+      </div>
+    <Table dataSource={sortedTransaction} columns={columns} />
+    </div>
     </>
   )
 }
